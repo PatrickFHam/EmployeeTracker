16ca60d4979df05ac51c4f093932ea2af53aca9b
PatrickFHam <93452044+PatrickFHam@users.noreply.github.com>
2022-03-25 00:10:12 -0400

    modeling some queries

--- index.js
+++ index.js
@@ -23,8 +23,10 @@ const db = mysql.createConnection(
 let updatedDepartments;
 let updatedRoles;
 let updatedEmployees;
+let updatedManagers;
+let updatedEmptyRoles;
 
-db.connect(function (err) {
+/* db.connect(function (err) {
   if (err) {
     console.error(err);
     return;
@@ -44,7 +46,7 @@ db.connect(function (err) {
   })
   
   initialPrompt();
-})
+}) */
 
 
 /* 
@@ -72,7 +74,7 @@ async function showAllDepartments () {
     let showResults = results;
     console.log("  \nAll Departments are Shown Below:");
     console.log(cTable.getTable(showResults));
-    initialPrompt();
+    topPrompt();
     }
   });
 }
@@ -83,7 +85,7 @@ async function showAllRoles () {
     let showResults = results;
     console.log("  \nAll Roles are Shown Below:");
     console.log(cTable.getTable(showResults));
-    initialPrompt();
+    topPrompt();
     }
   });
 }
@@ -94,20 +96,48 @@ async function showAllEmployees () {
     let showResults = results;
     console.log("  \nAll Employees are Shown Below:");
     console.log(cTable.getTable(showResults));
-    initialPrompt();
+    topPrompt();
     }
   });
 }
 
+async function refreshLists() {
+  db.query("SELECT * from department", function (error, res) {
+    updatedDepartments = res.map(departments => ({ name: departments.name, value: departments.id }))
+  });
 
-async function initialPrompt() {
+  db.query("SELECT * from role", function (error, res) {
+    updatedRoles = res.map(roles => ({ name: roles.title, value: roles.id }));
+    // console.log(cTable.getTable(updatedRoles));
+  });
+
+  db.query("SELECT * from employee", function (error, res) {
+    // console.log(error, res);
+    updatedEmployees = res.map(employees => ({ name: `${employees.first_name} ${employees.last_name}`, value: employees.id }))
+    // console.log(cTable.getTable(updatedEmployees));
+  });
+
+  db.query("SELECT first_name, last_name, id FROM employee WHERE (role_id IN (SELECT manager_id FROM employee));", function (error, res) {
+    updatedManagers = res.map(managers => ({ name: `${managers.first_name} ${managers.last_name}`, value: managers.id}));
+    // console.log(cTable.getTable(updatedManagers));
+  });
+
+  db.query("SELECT role.id, title, salary FROM role LEFT JOIN employee ON role.id = employee.role_id WHERE employee.role_id IS NULL;", function (error, res) {
+    updatedEmptyRoles = res.map(emptyRoles => ({ id: `${emptyRoles.id}`, title: `${emptyRoles.title}`, salary: `${emptyRoles.salary}`, value: emptyRoles.role.id}));
+    console.log(cTable.getTable(updatedEmptyRoles));
+  })
+  return
+}
+
+
+async function topPrompt() {
   inquirer
     .prompt([
       {
         type: 'list',
         message: 'Which would you like to do?',
         name: 'showWhichGroup',
-        choices: ['Show All Departments', 'Show All Roles', 'Show All Employees'],
+        choices: ['Show All Departments', 'Show All Roles', 'Show All Employees', 'Show All Empty Roles', 'Add an Employee', "Delete an Employee"]
       },
     ])
     .then((data) => {
@@ -120,6 +150,12 @@ async function initialPrompt() {
           return showAllRoles();
         case "Show All Employees":
           return showAllEmployees();
+        case "Show All Empty Roles":
+          return showAllEmptyRoles();
+        case "Add an Employee":
+          return addEmployee();
+        case "Delete an Employee":
+          return deleteEmployee();
       }
     });
 }
@@ -135,8 +171,88 @@ async function addRole() {
 }
 
 
-async function addEmployee() {
+async function showAllEmptyRoles() {
+  db.query('SELECT role.id, title, salary FROM role LEFT JOIN employee ON role.id = employee.role_id WHERE employee.role_id IS NULL;', function (err, results) {
+    if (err) {console.log(err)} else {
+    updatedEmptyRoles = results;
+    console.log("  \nAll EMPTY Roles are Shown Below:");
+    console.log(cTable.getTable(updatedEmptyRoles));
+    topPrompt();
+    }
+  });
+};
 
+
+async function addEmployee() {
+  db.query("SELECT role.id, title, salary FROM role LEFT JOIN employee ON role.id = employee.role_id WHERE employee.role_id IS NULL;", function (error, res) {
+    updatedEmptyRoles = res.map(emptyRoles => ({ id: `${emptyRoles.id}`, title: `${emptyRoles.title}`, salary: `${emptyRoles.salary}`, value: emptyRoles.role.id}));
+    // console.log(cTable.getTable(updatedEmptyRoles));
+  });
+  
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: "What is the new employee's FIRST NAME?",
+        name: "empFirstName",
+      },
+      {
+        type: "input",
+        message: "What is the new employee's LAST NAME?",
+        name: "empLastName",
+      },
+      {
+        type: "list",
+        message: "What is the new employee's TITLE?",
+        name: "roleTitle",
+        choices: updatedEmptyRoles
+      },
+      {
+        type: "list",
+        message: "Who is the employee's MANAGER?",
+        name: "empManager",
+        choices: updatedManagers
+      }
+    ]).then(function (response) {
+      // console.log(response)
+
+      // let roleID = 
+
+      db.query("INSERT INTO employee SET ?",
+        {
+          first_name: response.empFirstName,
+          last_name: response.empLastName,
+          role_id: response.roleTitle,
+          manager_id: response.empManager
+        })
+    })
+}
+
+
+async function deleteEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Which employee would you like to DELETE?",
+        name: "deletedEmployee",
+        choices: updatedEmployees
+      }
+    ]).then(function (response) {
+      // console.log(response)
+
+      deletedEmployeeChosenID = response.deletedEmployee;
+
+      db.query("DELETE FROM employee WHERE employee.id = ?", deletedEmployeeChosenID);
+
+      db.query("SELECT * from employee", function (error, res) {
+        // console.log(error, res);
+        updatedEmployees = res.map(employees => ({ name: `${employees.first_name} ${employees.last_name}`, value: employees.id }))
+        // console.log(cTable.getTable(updatedEmployees));
+      });
+
+      topPrompt();
+    })
 }
 
 
@@ -146,7 +262,12 @@ async function updateEmployeeRole () {
 
 
 
+async function runApp(){
+  await refreshLists();
+  await topPrompt();
+};
 
+runApp();
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
